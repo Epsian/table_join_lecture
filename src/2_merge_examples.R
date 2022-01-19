2b26c0e12c090c118221633e7f4d5a310db2131f
Jared Joseph <jared.n.joseph@gmail.com>
2022-01-18 16:07:16 -0800

    re-do merge section

--- src/2_merge_examples.R
+++ src/2_merge_examples.R
@@ -1,9 +1,13 @@
 # to make example dataframes for merge practice
 
+# rbind example
+upper_rbind_df = data.frame("name" = c("Lorelai Gilmore", "Kiki", "Link"), "color" = c("Orange", "Red", "Green"), "hometown" = c("Stars Hollow", "Koriko", "Castle Town"), stringsAsFactors = FALSE)
+lower_rbind_df = data.frame("name" = c("Rory Gilmore", "Jiji", "Zelda"), "color" = c("Yellow", "Indigo", "Violet"), "hometown" = c("Stars Hollow", "Koriko", "Castle Town"), stringsAsFactors = FALSE)
+
 # cbind example
-e1_cbind = data.frame("name" = c("Lorelai Gilmore", "Kiki", "Link"), "color" = c("Orange", "Red", "Green"), "hometown" = c("Stars Hollow", "Koriko", "Castle Town"), stringsAsFactors = FALSE)
-e2_cbind = data.frame("name" = c("Rory Gilmore", "Jiji", "Zelda"), "color" = c("Yellow", "Indigo", "Violet"), "hometown" = c("Stars Hollow", "Koriko", "Castle Town"), stringsAsFactors = FALSE)
+left_cbind_df = data.frame("name" = c("Lorelai Gilmore", "Kiki", "Link"), "color" = c("Orange", "Red", "Green"), "hometown" = c("Stars Hollow", "Koriko", "Castle Town"), stringsAsFactors = FALSE)
+right_cbind_df = data.frame("name" = c("Rory Gilmore", "Jiji", "Zelda"), "color" = c("Yellow", "Indigo", "Violet"), "hometown" = c("Stars Hollow", "Koriko", "Castle Town"), stringsAsFactors = FALSE)
 
 # key merge example
-e1_key = data.frame("name" = c("Lorelai Gilmore", "Razputin Aquato", "Link"), "color" = c("Orange", "Green", "Green"), "hometown" = c("Stars Hollow", "Whispering Rock", "Castle Town"), stringsAsFactors = FALSE)
-e2_key = data.frame("name" = c("Rory Gilmore", "Jiji", "Zelda"), "color" = c("Yellow", "Indigo", "Violet"), "hometown" = c("Stars Hollow", "Koriko", "Castle Town"), stringsAsFactors = FALSE)
+left_merge_df = data.frame("name" = c("Lorelai Gilmore", "Razputin Aquato", "Link"), "color" = c("Orange", "Green", "Green"), "hometown" = c("Stars Hollow", "Whispering Rock", "Castle Town"), stringsAsFactors = FALSE)
+right_merge_df = data.frame("name" = c("Rory Gilmore", "Jiji", "Zelda"), "color" = c("Yellow", "Indigo", "Violet"), "hometown" = c("Stars Hollow", "Koriko", "Castle Town"), stringsAsFactors = FALSE)
